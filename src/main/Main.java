e724271febfc205d5748bc147c9d900345c19036
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-20 15:14:26 -0300

    fourth way

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,8 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        for(int a = 0; a < 10; a++){
-            System.out.println("-->" + a);
+        for(int a = 1, b = 10; a <= 10; a++, b--){
+            System.out.println(a + " - " + b);
         }
     }
 }
