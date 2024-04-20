2ce78ede0da6201a13bd1beaa6f88edaeea78bd8
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-20 15:04:53 -0300

    second way with some changes

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,10 +3,9 @@ package main;
 public class Main {
     public static void main(String[] args) {
         int a = 1;
-        int b = 10;
 
         while(a<= 10){
-            System.out.println(a++ + " - " + b--);
+            System.out.println(a++ + " - " + ((10 - a++)));
         }
     }
 }
