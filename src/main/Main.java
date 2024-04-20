a5e98ae615c47eeff1e1d5e98357df049344a79d
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-20 15:02:32 -0300

    first way to make the logic

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,11 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        
+        int a = 1;
+        int b = 10;
+
+        while(a<= 10){
+            System.out.println(a++ + " - " + b--);
+        }
     }
 }
